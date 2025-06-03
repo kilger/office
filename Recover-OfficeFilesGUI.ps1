c59c2492dd618a5a3aa2a10a6dd0b475b8c48155
Kilger <kilger@users.noreply.github.com>
2025-06-03 11:21:08 -0700

    Create Recover-OfficeFilesGUI.ps1
    Empowering Users with a PowerShell Tool for Easy Microsoft File Recovery

---
+++ Recover-OfficeFilesGUI.ps1
@@ -0,0 +1,3 @@
+# Recover-OfficeFilesGUI.ps1
+# PowerShell script with Windows Forms GUI to find and recover unsaved Microsoft Office files
+# Includes all Office file types, .wbk, new locations, Recover with date-appended filename and extension matching, File Type column, and dropdown
